85859c81b30e7aa8542441fdc342ab3a19309a12
BaoCaiH <caihongbao280996@gmail.com>
2023-04-16 14:10:16 +0200

    tune motion

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private int extraJumpsValue = 1;
-    [SerializeField] private float speed = 7f;
+    [SerializeField] private float speed = 8f;
     [SerializeField] private float jumpForce = 28f;
     [SerializeField] private float checkRadius = 0.5f;
     [SerializeField] private float wallSlidingSpeed = 5f;
