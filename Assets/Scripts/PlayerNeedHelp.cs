ebb058403c41c211e49d0bb0630cd159a72d38f1
BaoCaiH <46781181+BaoCaiH@users.noreply.github.com>
2023-04-21 09:49:44 +0200

    add remaining control help

--- Assets/Scripts/PlayerNeedHelp.cs
+++ Assets/Scripts/PlayerNeedHelp.cs
@@ -6,11 +6,11 @@ public class PlayerNeedHelp : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        GameObject helper = collision.gameObject;
-        Animator anim = helper.GetComponentInParent<Animator>();
-        AudioSource sfxHelp = helper.GetComponentInParent<AudioSource>();
-        if (helper.CompareTag("Helps"))
+        GameObject collided = collision.gameObject;
+        if (collided.CompareTag("Helps"))
         {
+            Animator anim = collided.GetComponentInParent<Animator>();
+            AudioSource sfxHelp = collided.GetComponentInParent<AudioSource>();
             anim.SetTrigger("help");
             sfxHelp.Play();
         }
